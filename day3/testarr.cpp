9a07b2c0a822653c24b76c689159fd0bf3e379ac
Sakai04 <134619114+Sakai04@users.noreply.github.com>
2025-02-25 02:45:11 +0900

    ㄴ

---
+++ day3/testarr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void insert (int idx, int num, int arr[], int& len){
+    for(int i= len; i > idx; i--){
+        arr[i] = arr[i-1];
+        arr[idx] = num;
+        len = len + 1;
+    }
+
+}
+
+void erase(int idx, int arr[], int& len){
+    len = len - 1;
+    for(int i = idx; i < len; i++){
+        arr[i] = arr[i + 1];
+    }
+}
+
+int main(void){
+    int arr[10] = {10, 50, 40, 30, 70, 20};
+    int len = 6;
+    insert(3, 60, arr, len);
+    erase(4, arr, len);
+}
